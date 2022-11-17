f08ec6a23bbb290851b84af4b19e4ff8772e5ef8
nirgluzman <nir.gluzman@gmail.com>
2022-11-17 18:45:37 +0100

    display results - a bit of styling

--- src/components/displayresults/DisplayResults.js
+++ src/components/displayresults/DisplayResults.js
@@ -7,16 +7,13 @@ export default function DisplayResults({ searchResults }) {
         {searchResults.length ? (
           searchResults.map((result) => (
             <li key={uuid()} className="result">
-              <h3
+              <p
                 className="articletitle"
                 onClick={() => window.open(result.url, "_blank")}
               >
                 {result.title}
-              </h3>
-              <p className="author">
-                <strong>Created by:</strong>
-                {result.author}
               </p>
+              <p className="author">Created by: {result.author} </p>
               <p className="date">
                 {new Date(result.created_at).toISOString().split("T")[0]}
               </p>
